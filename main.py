7aa5a778335b8a9f9023e959d63022021f384876
renanMM <r.mariano.m@gmail.com>
2026-04-25 23:45:37 -0300

    feat: Implementação das formas de pagamento

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from src.machine import Machine
 def main():
     # Teste de cadastro de usuário na máquina e exemplo de uso ======================================
     admins = {
-        "renan123" : ["senha123",0]
+        "renan123" : ["senha123",0],
+        "luiz" : ["senha", 1]
     }
     maquina = Machine(0, "sao paulo", admins)
 
     maquina.operational.update_stock("renan123", "senha123")
-    maquina.operational.update_stock("renan123", "senha123")
     maquina.operational.registrar_bebida_dosada("renan123", "senha123")
   
     maquina.operational.buy_drink()
+    print(maquina.operational.get_dosed_sales("luiz","senha"))
 
 
 if __name__ == "__main__":
